ba813e90eb8d1d2f0bf643cee7101ed8ec3fba32
EfraCL <efrainclulea@hotmail.com>
2024-12-22 22:58:39 +0100

    He terminado los últimos pasos del script. Queda hacer pruebas con un dataset de entrenamiento

--- script_funcion_ldc.R
+++ script_funcion_ldc.R
@@ -14,13 +14,60 @@ long.curve <- function(col.variable,
   
   # Segundo paso ----
   ## Ordenar los datos de más antiguo a más reciente
+  if(is.unsorted(col.time)){ # Importante comprobar antes si estan o no ordenados
+    indices <- sort(col.time, index.return = T) # Ordenamos la variable tiempo y lo almacenamos en una lista junto con los índices de posición
+    col.time <- indices$x # Actualizamos la variable tiempo
+    
+    col.variable <- col.variable[indices$ix] # Ordenamos también la variable de interés, para que mantener el dataset inicial
+  }
   
-  dataframe <- data.frame(V = col.variable,
-                          time = col.time)
-  
-  
-  
-  
-  
-  
+  # Tercer paso ----
+  ## Determinar si incluimos todos los segmentos en el cálculo o no
+  if(include.all == T){
+    values <- c()
+    
+    for(i in 1:(length(col.variable) - 1)){
+      
+      dif.variable <- col.variable[i + 1] - col.variable[i]
+      dif.time <- is.numeric(col.time[i + i] - col.time[i])
+      values[i] <- sqrt(dif.time^2 + dif.variable^2)
+    }
+    longitudCurva <- sum(values, na.rm = T)
+    rm(values, dif.variable, dif.time, i)
+    
+    if(cal.GVI == T){
+      dif.variable <- col.variable[length(col.variable)] - col.variable[1]
+      dif.time <- is.numeric(col.time[length(col.time)] - col.time[1])
+      value <- sqrt(dif.time^2 + dif.variable^2)
+      
+      print(longitudCurva/value)
+    } else {
+      print(longitudCurva)
+    }
+    
+  } else {
+    values <- c()
+    for(i in 1: (length(col.variable)- 1)){
+      dif.time <- is.numeric(col.time[i + i] - col.time[i])
+      
+      if(time.measure == dif.time){
+        dif.variable <- col.variable[length(col.variable)] - col.variable[1]
+        values[i] <- sqrt(dif.time^2 + dif.variable^2)
+      } else {
+        next
+      }
+    }
+    longitudCurva <- sum(values, na.rm = T)
+    rm(values, dif.variable, dif.time, i)
+    
+    if(cal.GVI == T){
+      dif.variable <- col.variable[length(col.variable)] - col.variable[1]
+      dif.time <- is.numeric(col.time[length(col.time)] - col.time[1])
+      value <- sqrt(dif.time^2 + dif.variable^2)
+      
+      print(longitudCurva/value)
+    } else {
+      print(longitudCurva)
+    }
+  }
 }
