744bb535d0e37fe6da3fb515187f1f5a1d3dc33f
EfraCL <efrainclulea@hotmail.com>
2025-01-25 18:52:53 +0100

    He eliminado los prints de la función, porque de la otra manera se repetían las salidas de los resultados

--- script_funcion_ldc.R
+++ script_funcion_ldc.R
@@ -43,9 +43,9 @@ long.curve <- function(col.variable,
       dif.time <- as.numeric(difftime(col.time[length(col.time)], col.time[1], units = units.time))
       value <- sqrt(dif.time^2 + dif.variable^2)
       
-      print(longitudCurva/value)
+      longitudCurva/value
     } else {
-      print(longitudCurva)
+      longitudCurva
     }
     
   } else {
@@ -70,9 +70,9 @@ long.curve <- function(col.variable,
       dif.time <- as.numeric(difftime(col.time[length(col.time)], col.time[1], units = units.time))
       value <- sqrt(dif.time^2 + dif.variable^2)
       
-      print(longitudCurva/value)
+      longitudCurva/value
     } else {
-      print(longitudCurva)
+      longitudCurva
     }
   }
 }
